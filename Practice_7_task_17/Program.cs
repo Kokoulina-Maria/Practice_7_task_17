16d08ba15e4949fa169c2436e121046b0333f347
Maria <Maria@LAPTOP-N156UH30>
2017-08-23 21:12:16 +0500

    Добавьте файлы проекта.

---
+++ Practice_7_task_17/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_7_task_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
